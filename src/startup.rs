b393ef8fecd1cd132b03464d541c766e077338e7
Stephen Gates <sgates786@gmail.com>
2023-02-21 14:04:23 -0600

    finished chapter 3!

--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,19 @@
-use actix_web::{dev::Server, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
-        .listen(listener)?
-        .run();
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // wrap in arc pointer
+    let connection = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
+        App::new()
+            .service(health_check)
+            .service(subscribe)
+            .app_data(connection.clone())
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
