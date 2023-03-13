d806bc1e731ace2fc2795028562c6cc8fcd4534f
Stephen Gates <sgates786@gmail.com>
2023-03-13 13:59:14 -0500

    working

--- src/configuration.rs
+++ src/configuration.rs
@@ -54,21 +54,27 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let mut settings = config::Config::default();
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
-    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "local".into())
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT.");
-    settings.merge(
-        config::File::from(configuration_directory.join(environment.as_str())).required(true),
-    )?;
-    // Add in settings from environment variables (with a prefix of APP and '__' as separator)
-    // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
-    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
-    settings.try_into()
+    let environment_filename = format!("{}.yml", environment.as_str());
+    let settings = config::Config::builder()
+        .add_source(config::File::from(configuration_directory.join("base.yml")))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
+        ))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
 }
 
 /// The possible runtime environment for our application.
