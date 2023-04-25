5cb9845f9c28c5e49e5c9b45ade182ae26fd4962
Stephen Gates <sgates786@gmail.com>
2023-04-24 22:10:41 -0500

    "end" of ch.6 tests passing

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -29,8 +29,9 @@ impl std::fmt::Display for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claims::assert_err;
-    // use fake::faker::internet::en::SafeEmail;
-    // use fake::Fake;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+    use rand::{rngs::StdRng, SeedableRng};
 
     #[test]
     fn empty_string_is_rejected() {
@@ -53,15 +54,16 @@ mod tests {
     #[derive(Debug, Clone)]
     struct ValidEmailFixture(pub String);
 
-    // impl quickcheck::Arbitrary for ValidEmailFixture {
-    //     fn arbitrary(g: &mut G) -> Self {
-    //         let email = SafeEmail().fake_with_rng(g);
-    //         Self(email)
-    //     }
-    // }
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
 
-    // #[quickcheck_macros::quickcheck]
-    // fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
-    //     SubscriberEmail::parse(valid_email.0).is_ok()
-    // }
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,13 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string =
+        std::env::var("DATABASE_URL").expect("Failed to find DATABASE_URL in environment");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
+        // .connect_lazy_with(configuration.database.with_db());
+        .connect_lazy(&connection_string)
+        .expect("couldn't connect");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
